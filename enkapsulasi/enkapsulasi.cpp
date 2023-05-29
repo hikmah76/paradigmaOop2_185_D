a86539053a20b0643952e3066335971b4c3863ba
nur hikmah <nur.hikmah190504@gmail.com>
2023-05-29 17:24:38 +0700

    int main() {  seseorang* obyek;  joko a;  lia b;
     obyek = &a;
     obyek->pesan();
     obyek = &b;
     obyek->pesan();
     //a. seseorang::pesan();
     //b. seseorang::pesan();
    
    }

--- enkapsulasi/enkapsulasi.cpp
+++ enkapsulasi/enkapsulasi.cpp
@@ -9,7 +9,15 @@ public:
 		saklarNo[i] = value;
 	}
 	string getSaklarNo(int i) {
-		return
+		return saklarNo[i];
 	}
 };
 
+int main() {
+	remoteLampu lampuRumah;
+
+	lampuRumah.setSaklarNo(0, "Lampu Teras Rumah");
+	lampuRumah.setSaklarNo(1, "Lampu Ruang Tamu");
+	lampuRumah.setSaklarNo(2, "Lampu Kamar Tidur");
+	lampuRumah.setSaklarNo(0, "Lampu Teras Rumah");
+}
