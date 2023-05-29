c34bed64bb0efdfa1cb2554dd7f81c1ee671b485
nur hikmah <nur.hikmah190504@gmail.com>
2023-05-29 16:37:25 +0700

    #include <iostream> using namespace std;
    class orang {
    public:
     int umur;

--- multipewarisan/multipewarisan.cpp
+++ multipewarisan/multipewarisan.cpp
@@ -18,5 +18,37 @@ public:
 class manusia {
 public:
 	string jenisKelamin;
+
+	manusia(string pJenisKelamin) :
+		jenisKelamin(pJenisKelamin) {
+		cout << "manusia dibuat\n" << endl;
+	}
+	~manusia() {
+		cout << "manusia dihapus\n" << endl;
+	}
 };
+
+class pelajar : private manusia, public orang {
+public:
+	string sekolah;
+
+	pelajar(string pNama, string pJenisKelamin, string pSekolah) :
+		orang(pNama),
+		manusia(pJenisKelamin),
+		sekolah(pSekolah) {
+		cout << "pelajar dibuat\n" << endl;
+	}
+	~pelajar() {
+		cout << "pelajar dihapus\n" << endl;
+	}
+	string perkenalan() {
+		return "Halo, nama saya" + nama + "dengan jenis kelamin " + jenisKelamin + "dari sekolah" + sekolah + "\n\n";
+	}
+};
+
+int main() {
+	pelajar andi("andi laksono", "Laki-laki", "pelajarcpp");
+	cout << andi.perkenalan();
+	return 0;
+}
 
