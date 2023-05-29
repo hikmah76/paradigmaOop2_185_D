e0302ba180b0a1c3be0051b8a3b89a4051d786e9
nur hikmah <nur.hikmah190504@gmail.com>
2023-05-29 14:52:31 +0700

    Add project files.

---
+++ multipewarisan/multipewarisan.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class orang {
+public:
+	string nama;
+
+	orang(string pNama) :
+		nama(pNama) {
+		cout << "orang dibuat\n" << endl;
+	}
+	~orang() {
+		cout << "orang dibuat\n" << endl;
+	}
+};
+
+class manusia {
+public:
+	string jenisKelamin;
+};
+
